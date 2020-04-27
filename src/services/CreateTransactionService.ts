b08b894d5f8b674ded74d7b0cd774bc20924ecd0
roberto <betosimo@gmail.com>
2020-04-26 21:16:04 -0300

    backend finalized

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!["income", "outcome"].includes(type)) {
+      throw Error("type invalid");
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw Error("you dont have enough balance");
+
+
+    }
+
+    const transaction = this.transactionsRepository.create(
+      {
+        title: title,
+        value: value,
+        type: type
+      }
+    );
+
+    return transaction;
+
   }
 }
 
